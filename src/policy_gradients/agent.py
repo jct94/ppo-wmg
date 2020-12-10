learning implem of transformer archi; fix gradient flow through encodings

src/policy_gradients/agent.py
@@ -8,6 +8,7 @@ import numpy as np
 from copy import deepcopy
 import gym
 from .models import *
+from .transformer_models import *
 from .torch_utils import *
 from .steps import value_step, step_with_mode
 from .logging import *
@@ -71,50 +72,69 @@ class Trainer():
         self.n_steps = 0
         self.log_every = log_every
 
+        # Model
+        self.model = SceneTransformer(
+            obs_features=64,
+            n_actions=self.params.NUM_ACTIONS,
+            core_size=self.envs[0].observation_space[0],
+            factor_size=self.envs[0].observation_space[1]
+        )
+        self.max_tokens = self.model.max_objects
+        self.opt = optim.Adam(self.model.parameters(), lr=self.PPO_LR_ADAM)
+
         # Instantiation
-        self.policy_model = policy_net_class(self.NUM_FEATURES, self.NUM_ACTIONS,
-                                             self.INITIALIZATION,
-                                             time_in_state=time_in_state, share_weights=self.params.SHARE_WEIGHTS)
-
-        opts_ok = (self.PPO_LR == -1 or self.PPO_LR_ADAM == -1)
-        assert opts_ok, "One of ppo_lr and ppo_lr_adam must be -1 (off)."
-        # Whether we should use Adam or simple GD to optimize the policy parameters
-        if self.PPO_LR_ADAM != -1:
-            kwargs = {
-                'lr': self.PPO_LR_ADAM,
-            }
-
-            if self.params.ADAM_EPS > 0:
-                kwargs['eps'] = self.ADAM_EPS
-
-            self.params.POLICY_ADAM = optim.Adam(self.policy_model.parameters(),
-                                                 **kwargs)
-        else:
-            self.params.POLICY_ADAM = optim.SGD(self.policy_model.parameters(), lr=self.PPO_LR)
-
-        # If using a time dependent value function, add one extra feature
-        # for the time ratio t/T
-        if time_in_state:
-            self.params.NUM_FEATURES = self.NUM_FEATURES + 1
-
-        # Value function optimization
-        self.val_model = value_net_class(self.NUM_FEATURES, self.INITIALIZATION)
-        self.val_opt = optim.Adam(self.val_model.parameters(), lr=self.VAL_LR, eps=1e-5)
-        assert self.policy_model.discrete == (self.AGENT_TYPE == "discrete")
-
-        # Learning rate annealing
-        # From OpenAI hyperparametrs:
-        # Set adam learning rate to 3e-4 * alpha, where alpha decays from 1 to 0 over training
-        if self.ANNEAL_LR:
-            lam = lambda f: 1 - f / self.TRAIN_STEPS
-            ps = optim.lr_scheduler.LambdaLR(self.POLICY_ADAM,
-                                             lr_lambda=lam)
-            vs = optim.lr_scheduler.LambdaLR(self.val_opt, lr_lambda=lam)
-            self.params.POLICY_SCHEDULER = ps
-            self.params.VALUE_SCHEDULER = vs
+        # self.policy_model = policy_net_class(self.NUM_FEATURES, self.NUM_ACTIONS,
+        #                                      self.INITIALIZATION,
+        #                                      time_in_state=time_in_state, share_weights=self.params.SHARE_WEIGHTS)
+        #
+        # opts_ok = (self.PPO_LR == -1 or self.PPO_LR_ADAM == -1)
+        # assert opts_ok, "One of ppo_lr and ppo_lr_adam must be -1 (off)."
+        # # Whether we should use Adam or simple GD to optimize the policy parameters
+        # if self.PPO_LR_ADAM != -1:
+        #     kwargs = {
+        #         'lr': self.PPO_LR_ADAM,
+        #     }
+        #
+        #     if self.params.ADAM_EPS > 0:
+        #         kwargs['eps'] = self.ADAM_EPS
+        #
+        #     self.params.POLICY_ADAM = optim.Adam(self.policy_model.parameters(),
+        #                                          **kwargs)
+        # else:
+        #     self.params.POLICY_ADAM = optim.SGD(self.policy_model.parameters(), lr=self.PPO_LR)
+        #
+        # # If using a time dependent value function, add one extra feature
+        # # for the time ratio t/T
+        # if time_in_state:
+        #     self.params.NUM_FEATURES = self.NUM_FEATURES + 1
+        #
+        # # Value function optimization
+        # self.val_model = value_net_class(self.NUM_FEATURES, self.INITIALIZATION)
+        # self.val_opt = optim.Adam(self.val_model.parameters(), lr=self.VAL_LR, eps=1e-5)
+        # assert self.policy_model.discrete == (self.AGENT_TYPE == "discrete")
+        #
+        # # Learning rate annealing
+        # # From OpenAI hyperparametrs:
+        # # Set adam learning rate to 3e-4 * alpha, where alpha decays from 1 to 0 over training
+        # if self.ANNEAL_LR:
+        #     lam = lambda f: 1 - f / self.TRAIN_STEPS
+        #     ps = optim.lr_scheduler.LambdaLR(self.POLICY_ADAM,
+        #                                      lr_lambda=lam)
+        #     vs = optim.lr_scheduler.LambdaLR(self.val_opt, lr_lambda=lam)
+        #     self.params.POLICY_SCHEDULER = ps
+        #     self.params.VALUE_SCHEDULER = vs
 
         if store is not None:
             self.setup_stores(store)
+
+    def cpu_tensorize_v2(self, obss):
+        # this version uses the observation encoders from the model
+        tlist = []
+        for obs_list in obss:
+            tlist.append(self.model.encode_observations(obs_list))
+
+        return torch.stack(tlist)
+        # return self.model.encode_observations(t) # TODO check this works
 
     def setup_stores(self, store):
         # Logging setup
@@ -185,13 +205,14 @@ class Trainer():
         return advantages.clone().detach(), returns.clone().detach()
 
     def reset_envs(self, envs):
-        '''
+        """
         Resets environments and returns initial state with shape:
         (# actors, 1, ... state_shape)
-	    '''
+        """
         if self.CPU:
-            return cpu_tensorize([env.reset() for env in envs]).unsqueeze(1)
+            return self.cpu_tensorize_v2([env.reset() for env in envs]).unsqueeze(1)
         else:
+            raise ValueError("GPU not yet supported")
             return cu_tensorize([env.reset() for env in envs]).unsqueeze(1)
 
     def multi_actor_step(self, actions, envs):
@@ -219,10 +240,16 @@ class Trainer():
             # Aggregate
             normed_rewards.append([normed_reward])
             not_dones.append([int(not is_done)])
-            states.append([new_state])
+            states.append(new_state)
 
-        tensor_maker = cpu_tensorize if self.CPU else cu_tensorize
-        data = list(map(tensor_maker, [normed_rewards, states, not_dones]))
+        # tensor_maker = cpu_tensorize if self.CPU else cu_tensorize
+        # tensor_maker = self.cpu_tensorize_v2
+        data = [
+            cpu_tensorize(normed_rewards),
+            self.cpu_tensorize_v2(states),
+            cpu_tensorize(not_dones)
+        ]
+        # data = list(map(tensor_maker, [normed_rewards, states, not_dones]))
         return [completed_episode_info, *data]
 
     def run_trajectories(self, num_saps, return_rewards=False, should_tqdm=False):
@@ -263,19 +290,25 @@ class Trainer():
 
         assert self.NUM_ACTORS == 1
 
-        states[:, 0, :] = initial_states
-        last_states = states[:, 0, :]
+        states[:, 0, :, :] = initial_states
+        last_states = states[:, 0, :, :]
         for t in iterator:
             # assert shape_equal([self.NUM_ACTORS, self.NUM_FEATURES], last_states)
             # Retrieve probabilities 
             # action_pds: (# actors, # actions), prob dists over actions
             # next_actions: (# actors, 1), indices of actions
             # next_action_probs: (# actors, 1), prob of taken actions
-            action_pds = self.policy_model(last_states)
-            next_actions = self.policy_model.sample(action_pds)
-            next_action_log_probs = self.policy_model.get_loglikelihood(action_pds, next_actions)
+            action_pds = self.model(last_states)
+            next_actions = self.model.sample(action_pds)
+            next_action_log_probs = self.model.get_loglikelihood(action_pds, next_actions)
 
             next_action_log_probs = next_action_log_probs.unsqueeze(1)
+
+            # action_pds = self.policy_model(last_states)
+            # next_actions = self.policy_model.sample(action_pds)
+            # next_action_log_probs = self.policy_model.get_loglikelihood(action_pds, next_actions)
+
+            # next_action_log_probs = next_action_log_probs.unsqueeze(1)
             # shape_equal([self.NUM_ACTORS, 1], next_action_log_probs)
 
             # if discrete, next_actions is (# actors, 1) 
@@ -310,7 +343,10 @@ class Trainer():
                 (states, next_states)
             ]
 
-            last_states = next_states[:, 0, :]
+            # last_states = next_states[:, 0, :]
+            last_states = next_states # TODO: this is a quick fix, but in the original code the states
+                                      # have an additional dimension
+
             for total, v in pairs:
                 if total is states:
                     total[:, t + 1] = v
@@ -328,7 +364,7 @@ class Trainer():
             avg_episode_reward = -1
 
         # Last state is never acted on, discard
-        states = states[:, :-1, :]
+        states = states[:, :-1, :, :]
         trajs = Trajectories(rewards=rewards,
                              action_log_probs=action_log_probs, not_dones=not_dones,
                              actions=actions, states=states)
@@ -356,10 +392,13 @@ class Trainer():
             # value network, we use the get_value function of the 
             # *policy* to # estimate the value, instead of using the value
             # net
-            if not self.SHARE_WEIGHTS:
-                values = self.val_model(trajs.states).squeeze(-1)
-            else:
-                values = self.policy_model.get_value(trajs.states).squeeze(-1)
+            states = trajs.states.squeeze(0)
+            values = self.model.get_value(states).squeeze(-1)
+            values = values.unsqueeze(0)
+            # if not self.SHARE_WEIGHTS:
+            #     values = self.val_model(trajs.states).squeeze(-1)
+            # else:
+            #     values = self.policy_model.get_value(trajs.states).squeeze(-1)
 
             # Calculate advantages and returns
             advantages, returns = self.advantage_and_return(trajs.rewards,
@@ -427,8 +466,8 @@ class Trainer():
         # Take optimizer steps
         args = [saps.states, saps.actions, saps.action_log_probs,
                 saps.rewards, saps.returns, saps.not_dones,
-                saps.advantages, self.policy_model, self.params,
-                store_to_pass, self.n_steps]
+                saps.advantages, self.model, self.params,
+                store_to_pass, self.n_steps, self.opt]
 
         self.MAX_KL += self.MAX_KL_INCREMENT
 
@@ -442,9 +481,9 @@ class Trainer():
 
         # If the anneal_lr option is set, then we decrease the 
         # learning rate at each training step
-        if self.ANNEAL_LR:
-            self.POLICY_SCHEDULER.step()
-            self.VALUE_SCHEDULER.step()
+        # if self.ANNEAL_LR:
+        #     self.POLICY_SCHEDULER.step()
+        #     self.VALUE_SCHEDULER.step()
 
         if should_adv_log:
             log_value_losses(self, val_saps, 'heldout')
@@ -481,16 +520,16 @@ class Trainer():
         print("Surrogate Loss:", surr_loss.item(),
               "| Value Loss:", val_loss.item())
         print("Time elapsed (s):", time.time() - start_time)
-        if not self.policy_model.discrete:
-            mean_std = ch.exp(self.policy_model.log_stdev).mean()
-            print("Agent stdevs: %s" % mean_std)
-            self.store.log_table_and_tb('optimization', {
-                'mean_std': mean_std
-            })
-        else:
-            self.store['optimization'].update_row({
-                'mean_std': np.nan
-            })
+        # if not self.policy_model.discrete:
+        #     mean_std = ch.exp(self.policy_model.log_stdev).mean()
+        #     print("Agent stdevs: %s" % mean_std)
+        #     self.store.log_table_and_tb('optimization', {
+        #         'mean_std': mean_std
+        #     })
+        # else:
+        self.store['optimization'].update_row({
+            'mean_std': np.nan
+        })
 
         self.store['optimization'].flush_row()
         # End logging code

src/policy_gradients/transformer_models.py
@@ -1,9 +1,12 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 """
 This module defines the transformer models.
 """
+
+tensor = torch.FloatTensor
 
 # utility models
 
@@ -44,11 +47,246 @@ class MLP_TwoLayers_Norm(nn.Module):
 
 # main model
 
+class Transformer(nn.Module):
+    """
+    Wrapper around the pytorch Transformer module.
+    """
+    def __init__(self, n_heads, d_model, num_layers, dim_feedforward,
+                 dropout=0., query_token=0):
+        super().__init__()
+
+        # this assumes the core vector in in zeroth position
+        self.query_token = query_token
+
+        tfm_layer = nn.TransformerEncoderLayer(
+            d_model,
+            n_heads,
+            dim_feedforward,
+            dropout
+        )
+        norm = nn.LayerNorm(d_model)
+        self.tfm = nn.TransformerEncoder(tfm_layer, num_layers, norm)
+        # TODO: no custom weight init
+
+    def forward(self, input):
+        # input has size [seq, batch, feature_dim]
+        # we output the transformer value corresponding to the fixed query index
+        return self.tfm(input)[self.query_token]
+
+
 class SceneTransformer(nn.Module):
     """
-    Contains an encoding linear layer for scenes, a transformer layer
-    for relating the different objects, and policy and value
-    linear heads.
+    Contains a transformer layer
+    for relating the different objects, a policy head that is a linear
+    projection of the 0th output of the transformer, and a value head that
+    is a linear projection of the policy head.
     """
-    def __init__(self, ):
+    def __init__(self, obs_features, n_actions, core_size, factor_size):
 
+        super().__init__()
+
+        self.query_token = 0
+        self.max_objects = 9
+
+        # observation encodings
+        self.obs_features = obs_features
+        self.core_embedding = nn.Linear(core_size, obs_features)
+        self.factor_embedding = nn.Linear(factor_size, obs_features)
+
+        tfm_layer = nn.TransformerEncoderLayer(
+            obs_features,
+            1,
+            64,
+            0.
+        )
+        norm = nn.LayerNorm(obs_features)
+        self.tfm = nn.TransformerEncoder(tfm_layer, 1, norm)
+        self.mlp = MLP([obs_features, 64, 64])
+        self.policy_proj = nn.Linear(64, n_actions)
+        self.value_proj = nn.Linear(64, 1)
+
+    def encode_observations(self, obs_list):
+        """
+        This function takes the output of the BabyAI step and computes
+        a tensor version of the observation. Importantly, this is a method
+        of the transformer so that the parameters of these embeddings are
+        registered inside the model and accessible to the optimizer.
+        """
+        n = len(obs_list)
+        m = self.max_objects - n
+        if m < 0:
+            print(f"Dropped {- m} objects !")
+            obs_list = obs_list[:self.max_objects]
+
+        # encode core
+        vecs = [self.core_embedding(tensor(obs_list[0])).unsqueeze(0)]
+
+        # encode factors, if any
+        if obs_list[1:]:
+            for obs in obs_list[1:]:
+                vecs.append(self.factor_embedding(tensor(obs)).unsqueeze(0))
+
+        # pad with zeros if necessary
+        if m > 0:
+            vecs.append(torch.zeros(m, self.obs_features))
+
+        obs_tensor = torch.cat(vecs, 0)
+        return obs_tensor
+
+    def forward(self, seq_input):
+        # input has dim [batch, seq, in_features], a transposition is needed
+        seq_input = seq_input.transpose(0, 1)
+        tfm_out = self.tfm(seq_input)[self.query_token]
+        out = F.relu(self.mlp(tfm_out))
+        probs = F.softmax(self.policy_proj(out))
+
+        return probs
+
+    def get_value(self, seq_input):
+        # input has dim [seq, batch, in_features]
+        seq_input = seq_input.transpose(0, 1)
+        tfm_out = self.tfm(seq_input)[self.query_token]
+        out = F.relu(self.mlp(tfm_out))
+        value = self.value_proj(out)
+
+        return value
+
+    # utilities
+
+    def calc_kl(self, p, q, get_mean=True): # TODO: does not return a list
+        '''
+        Calculates E KL(p||q):
+        E[sum p(x) log(p(x)/q(x))]
+        Inputs:
+        - p, first probability distribution (NUM_SAMPLES, NUM_ACTIONS)
+        - q, second probability distribution (NUM_SAMPLES, NUM_ACTIONS)
+        Returns:
+        - Empirical KL from p to q
+        '''
+        p, q = p.squeeze(), q.squeeze()
+        # assert shape_equal_cmp(p, q)
+        kl = (p * (torch.log(p) - torch.log(q))).sum(-1)
+        return kl
+
+    def entropies(self, p):
+        '''
+        p is probs of shape (batch_size, action_space). return mean entropy
+        across the batch of states
+        '''
+        entropies = (p * torch.log(p)).sum(dim=1)
+        return entropies
+
+    def get_loglikelihood(self, p, actions):
+        '''
+        Inputs:
+        - p, batch of probability tensors
+        - actions, the actions taken
+        '''
+        try:
+            if len(actions)>1:
+                actions = actions[:,0]
+            dist = torch.distributions.categorical.Categorical(p)
+            return dist.log_prob(actions)
+        except Exception as e:
+            raise ValueError("Numerical error")
+
+    def sample(self, probs):
+        '''
+        given probs, return: actions sampled from P(.|s_i), and their
+        probabilities
+        - s: (batch_size, state_dim)
+        Returns actions:
+        - actions: shape (batch_size,)
+        '''
+        dist = torch.distributions.categorical.Categorical(probs)
+        actions = dist.sample()
+        return actions.long()
+
+# class TransformerDiscPolicy(DiscPolicy):
+#     """
+#     Prefixes the Fully-connected DiscPolicy with the Transformer module.
+#     """
+#     def __init__(self, state_dim, action_dim, init, # DiscPolicy args
+#                  n_heads, d_model, num_layers, dim_feedforward, # transformer args
+#                  hidden_sizes=HIDDEN_SIZES, share_weights=False, # DiscPolicy kwargs
+#                  dropout=0., query_token=0): # transformer kwargs
+#
+#         super().__init__(
+#             state_dim=state_dim,
+#             action_dim=action_dim,
+#             init=init,
+#             hidden_sizes=hidden_sizes,
+#             time_in_state=False,
+#             share_weights=True
+#         )
+#
+#         self.transformer = Transformer(
+#             n_heads=n_heads,
+#             d_model=d_model,
+#             num_layers=num_layers,
+#             dim_feedforward=dim_feedforward,
+#             dropout=dropout,
+#             query_token=query_token
+#         )
+#
+#     def forward(self, input):
+#         tfm_out = self.transformer(input)
+#         return super().forward(tfm_out)
+#
+#     def get_value(self, input):
+#         tfm_out = self.transformer(input)
+#         return super().get_value(tfm_out)
+#
+# class TransformerDiscPolicyv2(TransformerDiscPolicy):
+#     """
+#     This version takes in directly the states as output from the BabyAI custom env.
+#     """
+#     def __init__(self, state_dim, action_dim, init, # DiscPolicy args
+#                  n_heads, d_model, num_layers, dim_feedforward, # transformer args
+#                  hidden_sizes=HIDDEN_SIZES, share_weights=False, # DiscPolicy kwargs
+#                  dropout=0., query_token=0): # transformer kwargs
+#
+#         super().__init__(state_dim, action_dim, init,
+#                  n_heads, d_model, num_layers, dim_feedforward,
+#                  hidden_sizes=hidden_sizes, share_weights=share_weights,
+#                  dropout=dropout, query_token=query_token)
+#
+#         self.core_encoder = nn.Linear(63, d_model)
+#         self.factor_encoder = nn.Linear(23, d_model)
+#
+#     def encode_observation(self, obs_list):
+#         core_embedded = self.core_embedding(torch.FloatTensor(obs_list[0]))
+#         core_embedded = core_embedded.unsqueeze(0)
+#         if obs_list[1:]:
+#             factors_embedded = self.factor_embedding(cpu_tensorize(obs_list[1:]))
+#         else:
+#             factors_embedded = ch.zeros(0, self.model_size)
+#
+#         embedded_obs = ch.cat([core_embedded, factors_embedded], 0).unsqueeze(1)
+#
+#     def forward(self, obs_list):
+#         embedded_obs = self.encode_observation(obs_list)
+#         return super().forward(embedded_obs).squeeze(0)
+#
+#     def get_value(self, obs_list):
+#         embedded_obs = self.encode_observation(obs_list)
+#         return super().get_value(embedded_obs).squeeze(0)
+#
+# ## Retrieving networks
+# # Make sure to add newly created networks to these dictionaries!
+#
+# POLICY_NETS = {
+#     "DiscPolicy": DiscPolicy,
+#     "CtsPolicy": CtsPolicy
+# }
+#
+# VALUE_NETS = {
+#     "ValueNet": ValueDenseNet,
+# }
+#
+# def policy_net_with_name(name):
+#     return POLICY_NETS[name]
+#
+# def value_net_with_name(name):
+#     return VALUE_NETS[name]
+#